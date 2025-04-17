2ff8d755c4487570bd261985d7fef8ae08ba30cf
natnaelesk <natnaeleskinder41@gmail.com>
2025-04-17 09:03:52 +0300

    Apr 13 Answers

--- Python/Answers For Apr 14  2025/Prime_check.py
+++ Python/Answers For Apr 14  2025/Prime_check.py
@@ -3,11 +3,9 @@ import math
 def prime_check (n):
     if n < 2:
         return "Not Prime"
-
     for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
             return "Not Prime"
-
     return "Prime"
 
 print(prime_check(43))
